c654283febaed1a3ee0d589f98c0e118bb62e759
juel <juelruu@gmail.com>
2019-09-09 20:12:05 +0300

    Turret work like god say

--- BattleTanks/Source/BattleTanks/Private/TankTurret.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankTurret.cpp
@@ -3,11 +3,11 @@
 
 void UTankTurret::Rotate(float RelativeSpeed) {
 
-	auto Speed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+	auto RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
 
-	auto RotationChange = Speed * MaxDegPerSec * GetWorld()->DeltaTimeSeconds;
+	auto RotationChange = RelativeSpeed * MaxDegPerSec * GetWorld()->DeltaTimeSeconds;
 	auto Rotation = RelativeRotation.Yaw + RotationChange;
-	Rotation = FMath::Clamp<float>(Rotation, -179, +179);
+	//Rotation = FMath::Clamp<float>(Rotation, -179, +179);
 
 	SetRelativeRotation(FRotator(0, Rotation, 0));
 }
